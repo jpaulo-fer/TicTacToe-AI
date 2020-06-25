d03870360562fa6407d4f7d65d818a0d4f63f5ef
jpaulo-fer <jpaulo.valentin@gmail.com>
2020-06-25 18:20:08 -0300

    Correct all comments on tictactoe.py and add comments to player function.

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,4 @@
-"""
-Tic Tac Toe Player
-"""
-
+# Tic Tac Toe Player
 import math
 
 X = "X"
@@ -9,22 +6,21 @@ O = "O"
 EMPTY = None
 
 def initial_state():
+    # Returns starting state of the board.
 
-    """
-    Returns starting state of the board.
-    """
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
-    """
-    Returns player who has the next turn on a board.
-    """
+    # Returns player who has the next turn on a board.
+
+    # Variables for future comparison
     x_count = 0
     o_count = 0
 
+    # Iteration through list of lists (board) to store moves on the "count" variables.
     for i in board:
         for j in i:
             if j == X:
@@ -32,6 +28,7 @@ def player(board):
             elif j == O:
                 o_count += 1
 
+    # Compares both "count" variables and decides who has the next turn, being 1 for X and 2 for O.
     if x_count == o_count:
         return 1
     elif x_count > o_count:
@@ -39,42 +36,42 @@ def player(board):
 
 
 def actions(board):
-    """
-    Returns set of all possible actions (i, j) available on the board.
-    """
+  
+    # Returns set of all possible actions (i, j) available on the board.
+  
     raise NotImplementedError
 
 
 def result(board, action):
-    """
-    Returns the board that results from making move (i, j) on the board.
-    """
+
+    # Returns the board that results from making move (i, j) on the board.
+
     raise NotImplementedError
 
 
 def winner(board):
-    """
-    Returns the winner of the game, if there is one.
-    """
+
+    # Returns the winner of the game, if there is one.
+
     raise NotImplementedError
 
 
 def terminal(board):
-    """
-    Returns True if game is over, False otherwise.
-    """
+
+    # Returns True if game is over, False otherwise.
+
     raise NotImplementedError
 
 
 def utility(board):
-    """
-    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
-    """
+    
+    # Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    
     raise NotImplementedError
 
 
 def minimax(board):
-    """
-    Returns the optimal action for the current player on the board.
-    """
+
+    # Returns the optimal action for the current player on the board.
+    
     raise NotImplementedError
